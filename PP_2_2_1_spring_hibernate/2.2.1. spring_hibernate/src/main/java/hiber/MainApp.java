b14494d6495232ec0a6c01f9ff8f8b272561f265
Yaderkal <amesheryakov22813337@mail.ru>
2026-06-15 22:29:48 +0300

    :^)

--- PP_2_2_1_spring_hibernate/2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ PP_2_2_1_spring_hibernate/2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -14,11 +15,17 @@ public class MainApp {
             new AnnotationConfigApplicationContext(AppConfig.class);
 
       UserService userService = context.getBean(UserService.class);
-
       userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
       userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
       userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
       userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      userService.add(new Car( "Volvo", 2013));
+      userService.add(new Car( "Audi", 2018));
+      userService.add(new Car( "BYD", 2025));
+      userService.add(new Car( "UAZ", 1980));
+      userService.add(new Car( "MAN", 2006));
+
+
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -27,6 +34,12 @@ public class MainApp {
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
          System.out.println();
+      }
+      List<Car> cars = userService.listCars();
+      for (Car car : cars) {
+         System.out.println("Id = "+car.getId());
+         System.out.println("Id = "+car.getModel());
+         System.out.println("Id = "+car.getSeries());
       }
 
       context.close();
--- PP_2_2_1_spring_hibernate/2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ PP_2_2_1_spring_hibernate/2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -19,6 +19,10 @@ public class User {
    @Column(name = "email")
    private String email;
 
+   @OneToOne(cascade = CascadeType.ALL)
+   @JoinColumn(name = "car_id", unique = true)
+   private Car car;
+
    public User() {}
    
    public User(String firstName, String lastName, String email) {
