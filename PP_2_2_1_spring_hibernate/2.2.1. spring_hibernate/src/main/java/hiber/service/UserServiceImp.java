651479e8250fef17771f4fa191946fc106eace64
Yaderkal <amesheryakov22813337@mail.ru>
2026-06-21 21:25:04 +0300

    хз как протестить HQL запрос в сервисе))

--- PP_2_2_1_spring_hibernate/2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ PP_2_2_1_spring_hibernate/2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -3,6 +3,11 @@ package hiber.service;
 import hiber.dao.UserDao;
 import hiber.model.Car;
 import hiber.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.NativeQuery;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +18,8 @@ import java.util.List;
 public class UserServiceImp implements UserService {
 
    @Autowired
+   private SessionFactory sessionFactory;
+   @Autowired
    private UserDao userDao;
 
    @Transactional
@@ -20,6 +27,7 @@ public class UserServiceImp implements UserService {
    public void add(User user) {
       userDao.add(user);
    }
+
    @Transactional
    @Override
    public void add(Car car) {
@@ -31,10 +39,23 @@ public class UserServiceImp implements UserService {
    public List<User> listUsers() {
       return userDao.listUsers();
    }
+
    @Transactional(readOnly = true)
    @Override
    public List<Car> listCars() {
       return userDao.listCars();
    }
 
+   @Transactional
+   @Override
+   public List<User> getOwner(String model, int series) {
+      Session session = sessionFactory.getCurrentSession();
+      Query<User> query = session.createQuery(
+              "SELECT u FROM User u JOIN FETCH u.car c WHERE c.model = :model AND c.series = :series",
+              User.class
+      );
+      query.setParameter("model", model);
+      query.setParameter("series", series);
+      return query.list();
+   }
 }
